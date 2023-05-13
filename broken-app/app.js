0cf63a9249ec5c447f94ff45da612d1bff86e574
Ethan Reynolds <ethan.t.reynolds@gmail.com>
2023-05-13 17:15:49 -0600

    fix broken app

--- broken-app/app.js
+++ broken-app/app.js
@@ -8,17 +8,23 @@ const base_url = 'https://api.github.com/users'
 
 app.post('/', async function (req, res, next) {
   try {
-    let results = [];
+    let results = []
     for (dev of req.body.developers) {
       let resp = await axios.get(`${base_url}/${dev}`)
-      results.push({
-        "name": resp.data.name, "bio": resp.data.bio
-      })
+      results.push({ "name": resp.data.name, "bio": resp.data.bio })
     }
-    return res.send(results);
+    return res.send(results)
   } catch (err) {
     next(err);
   }
+});
+
+app.use(function (err, req, res, next) {
+  let status = err.status || 500;
+  let message = err.message;
+  return res.status(status).json({
+    error: { message, status }
+  });
 });
 
 app.listen(3000);
--- broken-app/issues.md
+++ broken-app/issues.md
@@ -1,1 +1,9 @@
 # Broken App Issues
+
+- Installed express and axios
+- Changed the variable declarations at the top of the js file to *const*
+- Added a *base_url* variable
+- Added *(err)* to *catch*
+- Added *app.use(expres.json());*
+- Rewrote the **POST /** function so that it retrieves the intended results and forms an array
+- Added a general error handler
