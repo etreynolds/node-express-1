9d648cdfef6378a764261ef3dee773613bf6a08d
Ethan Reynolds <ethan.t.reynolds@gmail.com>
2023-05-13 16:53:33 -0600

    install express

--- broken-app/app.js
+++ broken-app/app.js
@@ -1,16 +1,22 @@
 const express = require('express');
-let axios = require('axios');
-var app = express();
+const axios = require('axios');
+const app = express();
 
-app.post('/', function(req, res, next) {
+app.use(express.json());
+
+const base_url = 'https://api.github.com/users'
+
+app.post('/', async function (req, res, next) {
   try {
-    let results = req.body.developers.map(async d => {
-      return await axios.get(`https://api.github.com/users/${d}`);
-    });
-    let out = results.map(r => ({ name: r.data.name, bio: r.data.bio }));
-
-    return res.send(JSON.stringify(out));
-  } catch {
+    let results = [];
+    for (dev of req.body.developers) {
+      let resp = await axios.get(`${base_url}/${dev}`)
+      results.push({
+        "name": resp.data.name, "bio": resp.data.bio
+      })
+    }
+    return res.send(results);
+  } catch (err) {
     next(err);
   }
 });
